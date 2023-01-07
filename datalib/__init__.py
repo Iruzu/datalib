04bf4996f89c0a67fc4dfeaac8d4de2cac6b2463
Iruzu <58382526+Iruzu@users.noreply.github.com>
2023-01-07 18:15:25 +0100

    Update __init__.py

---
+++ datalib/__init__.py
@@ -0,0 +1,6 @@
+from datalib import correlation
+from datalib import discretization
+from datalib import feature_scaling
+from datalib import filtering
+from datalib import metrics
+from datalib import plotting
